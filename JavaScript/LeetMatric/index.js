426e151925004b1cb3a39ea98de504d0f5231a3d
PramodNishad <nishadpramod.001@gmail.com>
2025-04-27 12:31:38 +0530

    javascript completed

--- JavaScript/LeetMatric/index.js
+++ JavaScript/LeetMatric/index.js
@@ -1,4 +1,4 @@
-\document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () {
 
 
     const searchButton = document.getElementById("search-btn");
